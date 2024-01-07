60bfd650426f9ee00885e2892e863622981711c0
Luis Fernando <guffenix@gmail.com>
2024-01-07 13:50:14 -0500

    chore(impr): layout ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'MunayFund',
@@ -12,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={'font-poppins'}>{children}</body>
     </html>
   )
 }
