b7ed570f7926d1e47826151f4f97f8a4fc20bb89
Luis Fernando <guffenix@gmail.com>
2024-01-07 21:23:21 -0500

    chore: team members info

---
+++ src/constants/team-members.const.ts
@@ -0,0 +1,5 @@
+export const TEAM = [
+  { name: 'Wolfcito', image: '/team/wolfcito.png', repo: 'https://github.com/wolfcito' },
+  { name: 'Daniel', image: '/team/daniel.png', repo: 'https://github.com/daniel0ar' },
+  { name: 'Manuel', image: '/team/manuel.png', repo: 'https://github.com/ManuelLecaro' },
+]
